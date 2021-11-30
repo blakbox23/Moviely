28504c5a704cb08d4061503126a4406c6b803bb6
Peter MX <36844547+blakbox23@users.noreply.github.com>
2021-12-01 00:30:04 +0300

    Use App.txs as temporary container for MovieItem molecule

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+// import './App.css'
+import { MovieItem } from './components/UI/molecules/MovieItem/MovieItem'
 
 function App() {
+  const movies = [
+    {
+      id: 1,
+      title: 'Braver',
+      image: 'https://source.unsplash.com/cFbNlpRZKi0',
+      description:
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id quidem accusantium facere natus molestiae aspernatur ullam dicta rem at consectetur?',
+    },
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MovieItem
+        title={movies[0].title}
+        image={movies[0].image}
+        description={movies[0].description}
+      />
     </div>
   )
 }
