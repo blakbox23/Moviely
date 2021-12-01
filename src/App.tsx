77c631f01510ca45b479ba89769af2b8d03911a6
Peter MX <36844547+blakbox23@users.noreply.github.com>
2021-12-01 16:24:56 +0300

    Add the Nav component into App.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css'
 import Homebody from './components/UI/organisms/HomeBody/Homebody'
+import Nav from './components/UI/organisms/Nav/Nav'
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Homebody />
     </div>
   )
