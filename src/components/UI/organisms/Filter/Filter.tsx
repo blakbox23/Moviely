6cbc70f8398b3bf84f66efb80c8c6e0c201f1ef1
Peter MX <36844547+blakbox23@users.noreply.github.com>
2021-12-02 00:20:08 +0300

    Create filter organism

---
+++ src/components/UI/organisms/Filter/Filter.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './style.css'
+import Filterform from '../../molecules/Filterform/Filterform'
+
+function Filter() {
+  return (
+    <div className="filter-section">
+      <Filterform />
+    </div>
+  )
+}
+
+export default Filter
