4bd816676b86682d51c08bdba43dbfee6818644f
Peter MX <36844547+blakbox23@users.noreply.github.com>
2021-12-01 09:55:14 +0300

    Remove logic from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import React from 'react'
-// import './App.css'
-import { MovieItem } from './components/UI/molecules/MovieItem/MovieItem'
+import './App.css'
+import Homebody from './components/UI/organisms/HomeBody/Homebody'
 
 function App() {
-  const movies = [
-    {
-      id: 1,
-      title: 'Braver',
-      image: 'https://source.unsplash.com/cFbNlpRZKi0',
-      description:
-        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id quidem accusantium facere natus molestiae aspernatur ullam dicta rem at consectetur?',
-    },
-  ]
   return (
     <div className="App">
-      <MovieItem
-        title={movies[0].title}
-        image={movies[0].image}
-        description={movies[0].description}
-      />
+      <Homebody />
     </div>
   )
 }
