16accf7ed6f36e694c92b805eed0cfaa3b19a419
Peter MX <36844547+blakbox23@users.noreply.github.com>
2021-12-03 12:44:15 +0300

    Clean up homebody component

--- src/components/UI/organisms/HomeBody/Homebody.tsx
+++ src/components/UI/organisms/HomeBody/Homebody.tsx
@@ -6,19 +6,8 @@ import './style.css'
 import Filter from '../../organisms/Filter/Filter'
 
 function Homebody() {
-  const { pending, movies, error } = useSelector(
-    (state: RootState) => state.movies,
-  )
-
-  // console.log(movies)
-
   return (
     <div className="homebody">
-      {pending && error && <div>Loading...</div>}
-      {movies.map((movie) => (
-        <p>{movie.title}</p>
-      ))}
-
       <Filter />
       <MoviesList />
     </div>
