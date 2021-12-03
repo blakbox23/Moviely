208440a4adfc21e136b8da14a1dcb9446c3772e7
Peter MX <36844547+blakbox23@users.noreply.github.com>
2021-12-03 10:34:12 +0300

    Get state from the store using useSelector

--- src/components/UI/organisms/HomeBody/Homebody.tsx
+++ src/components/UI/organisms/HomeBody/Homebody.tsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { RootState } from '../../../../store/Reducers/rootReducers'
 import { MoviesList } from '../MoviesList/MoviesList'
 import './style.css'
 import Filter from '../../organisms/Filter/Filter'
 
 function Homebody() {
+  const { pending, movies, error } = useSelector(
+    (state: RootState) => state.movies,
+  )
+
+  // console.log(movies)
+
   return (
     <div className="homebody">
+      {pending && error && <div>Loading...</div>}
+      {movies.map((movie) => (
+        <p>{movie.title}</p>
+      ))}
+
       <Filter />
       <MoviesList />
     </div>
