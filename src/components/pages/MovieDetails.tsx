6042bc6063313f27653d16d2f19e8f8a44bcd587
Peter MX <36844547+blakbox23@users.noreply.github.com>
2021-12-03 22:08:22 +0300

    Set up react-router-dom and the Movie details page

---
+++ src/components/pages/MovieDetails.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function MovieDetails() {
+  return <div>This will be the details page</div>
+}
+
+export default MovieDetails
--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './store/Store'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root'),
 )
