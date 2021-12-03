7e42003bebaa65651b33d15345f3c3cdb37f95e3
Peter MX <36844547+blakbox23@users.noreply.github.com>
2021-12-03 10:30:46 +0300

    Implement types for redux and movie actions

--- src/store/Actions/MoviesAction.ts
+++ src/store/Actions/MoviesAction.ts
@@ -1,5 +1,30 @@
-export const GET_MOVIES = 'GET_MOVIES'
-export const GET_MOVIES_SUCCESS = 'GET_MOVIES_SUCCESS'
+import { movieTypes } from '../ActionTypes/Movietypes'
+import {
+    FetchmoviesFailure,
+    FetchmoviesFailurePayload,
+    Fetchmovies,
+    FetchmoviesSuccess,
+    FetchmoviesSuccessPayload
+  } from "../types/types";
 
-//action creator
-export const getMovies = () =>({type: GET_MOVIES})
+
+  //action creator
+ 
+  export const fetchMovies = (): Fetchmovies => ({
+    type: movieTypes.FETCH_MOVIES
+  });
+  
+  export const fetchmoviesSuccess = (
+    payload: FetchmoviesSuccessPayload
+  ): FetchmoviesSuccess => ({
+    type: movieTypes.FETCH_MOVIES_SUCCESS,
+    payload
+  });
+  
+  export const fetchmoviesFailure = (
+    payload: FetchmoviesFailurePayload
+  ): FetchmoviesFailure => ({
+    type: movieTypes.FETCH_MOVIES_FAILURE,
+    payload
+  });
+  
---
+++ src/store/types/types.ts
@@ -0,0 +1,42 @@
+import { movieTypes } from "../ActionTypes/Movietypes";
+
+export interface IMovie {
+    userId: number;
+    id: number;
+    title: string;
+    completed: boolean;
+  }
+  
+
+export interface MoviesState {
+  pending: boolean;
+  movies: IMovie[];
+  error: string | null;
+}
+
+export interface FetchmoviesSuccessPayload {
+  movies: IMovie[]; 
+}
+
+export interface FetchmoviesFailurePayload {
+  error: string;
+}
+
+export interface Fetchmovies {
+  type: typeof movieTypes.FETCH_MOVIES;
+}
+
+export type FetchmoviesSuccess = {
+  type: typeof movieTypes.FETCH_MOVIES_SUCCESS;
+  payload: FetchmoviesSuccessPayload;
+};
+
+export type FetchmoviesFailure = {
+  type: typeof movieTypes.FETCH_MOVIES_FAILURE;
+  payload: FetchmoviesFailurePayload;
+};
+
+export type MoviesActions =
+  | Fetchmovies
+  | FetchmoviesSuccess
+  | FetchmoviesFailure;
