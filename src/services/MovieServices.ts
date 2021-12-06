0b308f848f22fbf9999429ff5af2fe9194d0d052
Peter MX <36844547+blakbox23@users.noreply.github.com>
2021-12-06 20:46:18 +0300

    Connect movie details to services

--- src/services/MovieServices.ts
+++ src/services/MovieServices.ts
@@ -2,12 +2,17 @@ import ApiService from "./ApiServices";
 
 const ENDPOINTS = {
     MOVIES: '/movies',
+    MOVIE: '/movies/:id'
   };
 
   class MovieService extends ApiService {
 
     getMovies = (values: string) => {
         return this.apiClient.get(ENDPOINTS.MOVIES);  
+    }
+
+    getMoviebyid = (values: string) => {
+        return this.apiClient.get(ENDPOINTS.MOVIE);  
     }
 }
 
