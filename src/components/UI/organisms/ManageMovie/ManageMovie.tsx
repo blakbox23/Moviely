694389239d69a55da2878f1b238ff5a38a3cc795
Peter MX <36844547+blakbox23@users.noreply.github.com>
2021-12-08 10:17:05 +0300

    Set up the add movie form with the header

--- src/components/UI/organisms/ManageMovie/ManageMovie.tsx
+++ src/components/UI/organisms/ManageMovie/ManageMovie.tsx
@@ -1,34 +1,49 @@
 import React from 'react'
 import './style.css'
 import { Input } from '../../atoms/Input/Input'
+import { PageHeader } from '../../molecules/PageHeader/PageHeader'
 interface ManageMovieProps {}
 
 export const ManageMovie: React.FC<ManageMovieProps> = ({}) => {
   return (
     <div className="manage-movie">
-      <div className="manage-movie-container">
-        <div className="manage-movie-half-container">
-          <Input type="manage-movie-half" placeholder="my title" border />
-          <Input type="manage-movie-half" placeholder="my title" border />
-          <Input type="manage-movie-half" placeholder="my title" border />
-          <Input type="manage-movie-half" placeholder="my title" border />
-          <Input type="manage-movie-half" placeholder="my title" border />
-          <Input type="manage-movie-half" placeholder="my title" border />
+      <PageHeader text="New Movie" />
+      <form action="">
+        <div className="manage-movie-container">
+          <div className="manage-movie-half-container">
+            <Input type="manage-movie-half" placeholder="my title" border />
+
+            <select className="manage-movie-half genre-drop" name="genres">
+              <option value="genre">Genre</option>
+              <option value="action">Action</option>
+              <option value="horror">Horror</option>
+              <option value="comedy">Comedy</option>
+            </select>
+
+            <Input type="manage-movie-half" placeholder="Year" border />
+            <Input type="manage-movie-half" placeholder="Runtime" border />
+            <Input type="manage-movie-half" placeholder="Image Url" border />
+            <Input type="manage-movie-half" placeholder="Country" border />
+          </div>
+          <div>
+            <Input type="manage-movie-full" placeholder="Trailer Url" border />
+          </div>
+          <div>
+            <textarea
+              className="manage-movie-textarea"
+              placeholder="This is some sort of description of movie!"
+              name="description"
+              // cols={130}
+              // rows={10}
+            ></textarea>
+          </div>
+          <input
+            type="submit"
+            className="form-submit-button flex"
+            value="Submit"
+          />
         </div>
-        <div>
-          <Input type="manage-movie-full" placeholder="my title" border />
-        </div>
-        <div>
-          <textarea
-            className="manage-movie-textarea"
-            placeholder="This is some sort of description of movie!"
-            name="description"
-            // cols={130}
-            // rows={10}
-          ></textarea>
-        </div>
-        button
-      </div>
+      </form>
     </div>
   )
 }
