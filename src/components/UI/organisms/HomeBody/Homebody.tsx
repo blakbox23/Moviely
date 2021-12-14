ca8d8412176b6fa16a4eb7d119af792544b6503c
Peter MX <36844547+blakbox23@users.noreply.github.com>
2021-12-14 09:41:10 +0300

    Set up conditional rendering to either show all movies or filtered movies

--- src/components/UI/organisms/HomeBody/Homebody.tsx
+++ src/components/UI/organisms/HomeBody/Homebody.tsx
@@ -3,13 +3,19 @@ import { MoviesList } from '../MoviesList/MoviesList'
 import { FilteredMoviesList } from '../FilteredMoviesList/FilteredMoviesList'
 import './style.css'
 import Filter from '../../organisms/Filter/Filter'
+import { RootState } from '../../../../store/Reducers/rootReducers'
+import { useSelector } from 'react-redux'
 
 function Homebody() {
+  const { pending, searchedMovies, error } = useSelector(
+    (state: RootState) => state.movies,
+  )
   const filtered = false
   return (
     <div className="homebody">
       <Filter />
-      <MoviesList />
+
+      {searchedMovies.length === 0 ? <MoviesList /> : <FilteredMoviesList />}
     </div>
   )
 }
