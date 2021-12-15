67cc6520e0715476496bf01c47841750b825e755
Peter MX <36844547+blakbox23@users.noreply.github.com>
2021-12-15 10:45:51 +0300

    Prepare merge

--- src/store/Sagas/rootSaga.ts
+++ src/store/Sagas/rootSaga.ts
@@ -1,8 +1,9 @@
 import { all, fork } from "redux-saga/effects";
-import moviesSaga from "./MovieSagas.ts/MoviesSagas";
-import movieSaga from "./MovieSagas.ts/MovieByIdSaga";
-import CreateMovieSaga from "./MovieSagas.ts/CreateMovieSaga";
-import FilteredMoviesSaga from "./MovieSagas.ts/FilteredMoviesSaga";
+import moviesSaga from "./MovieSagas/MoviesSagas";
+import movieSaga from "./MovieSagas/MovieByIdSaga";
+import CreateMovieSaga from "./MovieSagas/CreateMovieSaga";
+import FilteredMoviesSaga from "./MovieSagas/FilteredMoviesSaga";
 
 export default function* rootSaga() {
-  yield all([fork(moviesSaga), fork(movieSaga), fork(CreateMovieSaga), fork(FilteredMoviesSaga)]);
+  yield all([fork(moviesSaga), fork(movieSaga), fork(CreateMovieSaga), fork(FilteredMoviesSaga)])
+}
