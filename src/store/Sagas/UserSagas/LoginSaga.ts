969e5d79aae423f2348ea0e52cd1aa76dc7a2014
Peter MX <36844547+blakbox23@users.noreply.github.com>
2022-02-25 12:50:24 +0300

    Set up login saga

---
+++ src/store/Sagas/UserSagas/LoginSaga.ts
@@ -0,0 +1,38 @@
+import { put, call, takeEvery, all } from 'redux-saga/effects';
+import { userTypes } from '../../ActionTypes/Usertypes'
+// import {notify, success} from '../../../components/UI/organisms/Toasts/Toast'
+import { LoginFailure, LoginSuccess } from '../../Actions/UserActions';
+import { userService } from '../../../services/UserServices';
+
+
+function* workLogin(action:any): any {
+  console.log('action.payload');
+  console.log(action.payload);
+    try {
+      const loginResponse = yield call(userService.getLogin, action.payload);
+
+      console.log('loginResponse')
+      console.log(loginResponse)
+
+
+      yield put(
+        LoginSuccess({
+            user: loginResponse
+          })
+        );
+  
+      } catch (e: any) {
+        yield put(
+          LoginFailure({
+            error: e.message
+          })
+        ); 
+      }
+    }
+  
+
+function* LoginSaga() {
+    yield all([takeEvery(userTypes.LOGIN, workLogin)])
+}
+
+export default LoginSaga;
