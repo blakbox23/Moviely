33756afac896bc98657d5cdee9406a8360310e94
Peter MX <36844547+blakbox23@users.noreply.github.com>
2022-03-09 16:34:16 +0300

    Set up fetch comments saga

---
+++ src/store/Sagas/MovieSagas/FetchCommentsSaga.ts
@@ -0,0 +1,32 @@
+import { put, call, takeLatest, all } from 'redux-saga/effects';
+import { fetchcommentsFailure, fetchcommentsSuccess } from '../../Actions/MoviesAction'
+import { movieService } from '../../../services/MovieServices'
+import { movieTypes } from '../../ActionTypes/Movietypes'
+
+
+function* workFetchComments(action: any): any {
+    try {
+       const response = yield call(movieService.getComments, action.payload);
+       console.log('fetch movie comments')
+       console.log(response.data)
+    yield put(
+        fetchcommentsSuccess({
+          movie_comments: response.data
+        })
+      );
+    } catch (e: any) {
+      yield put(
+        fetchcommentsFailure({
+          error: e.message
+        })
+      ); 
+    }
+  }
+
+function* commentsSaga() {
+    yield all([takeLatest(movieTypes.FETCH_COMMENTS, workFetchComments)])
+}
+
+
+
+export default commentsSaga;
