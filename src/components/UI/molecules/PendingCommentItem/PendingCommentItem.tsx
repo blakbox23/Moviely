3df8a2887e4f34b445594ac4c13053365237338b
Peter MX <36844547+blakbox23@users.noreply.github.com>
2022-03-09 19:50:57 +0300

    Update pending comments component

--- src/components/UI/molecules/PendingCommentItem/PendingCommentItem.tsx
+++ src/components/UI/molecules/PendingCommentItem/PendingCommentItem.tsx
@@ -1,56 +1,42 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Button from '../../atoms/Button/Button'
 import Text from '../../atoms/Text/Text'
 import './style.css'
 import comment from '../../../../assets/comment.png'
+import { useSelector } from 'react-redux'
+import { RootState } from '../../../../store/Reducers/rootReducers'
 
-interface PendingCommentItemProps {}
+interface PendingCommentItemProps {
+  id: string
+  email: string
+  content: string
+  movieName: string
+  approved: boolean
+}
 
-export const PendingCommentItem: React.FC<PendingCommentItemProps> = ({}) => {
+export const PendingCommentItem: React.FC<PendingCommentItemProps> = ({
+  id,
+  email,
+  content,
+  movieName,
+  approved,
+}) => {
+  const { pending, movies, error } = useSelector(
+    (state: RootState) => state.movies,
+  )
+
+  console.log('movies')
+  console.log(movies)
   return (
     <div>
       <div className="pending-comment-item flex">
         <div className="comment-details flex">
-          <div>Userasdsdse</div>
-          <div>Brave</div>
+          <div>{email}</div>
+          <div>{movieName}</div>
         </div>
         <div className="pending-comment-textcontainer">
           <img src={comment} alt="" />
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. At odit
-            debitis eligendi. Quidem voluptates quos ea qui magni consequatur
-            illo quibusdam aliquam nihil! Cum, sed!
-          </p>
-        </div>
-        <div className="pending-comments-buttons flex">
-          <div>
-            <Button
-              placement="watched-movies"
-              buttontext="Approve"
-              color="#F4242480"
-            />
-          </div>
-          <div>
-            <Button
-              placement="watched-movies"
-              buttontext="Deny"
-              color="#2596BE80"
-            />
-          </div>
-        </div>
-      </div>
-      <div className="pending-comment-item flex">
-        <div className="comment-details flex">
-          <div>Userasdsdse</div>
-          <div>Brave</div>
-        </div>
-        <div className="pending-comment-textcontainer">
-          <img src={comment} alt="" />
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. At odit
-            debitis eligendi. Quidem voluptates quos ea qui magni consequatur
-            illo quibusdam aliquam nihil! Cum, sed!
-          </p>
+          <p>{content}</p>
         </div>
         <div className="pending-comments-buttons flex">
           <div>
--- src/components/UI/organisms/PendingCommentsList/PendingCommentsList.tsx
+++ src/components/UI/organisms/PendingCommentsList/PendingCommentsList.tsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getAllComments } from '../../../../store/Actions/CommentsActions'
+import { RootState } from '../../../../store/Reducers/rootReducers'
 import { PageHeader } from '../../molecules/PageHeader/PageHeader'
 import { PendingCommentItem } from '../../molecules/PendingCommentItem/PendingCommentItem'
 import './style.css'
@@ -6,10 +9,39 @@ import './style.css'
 interface PendingCommentsListProps {}
 
 export const PendingCommentsList: React.FC<PendingCommentsListProps> = ({}) => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getAllComments())
+  }, [dispatch])
+
+  const { pending, comments, error } = useSelector(
+    (state: RootState) => state.comments,
+  )
+  const { movies } = useSelector((state: RootState) => state.movies)
+
+  console.log('movies')
+  console.log(movies)
+
   return (
     <div className="pending-comments-list">
       <PageHeader text="Pending comments" />
-      <PendingCommentItem />
+      {comments &&
+        comments.map((comment) => (
+          <div key={comment.id}>
+            <PendingCommentItem
+              id={comment.id}
+              email={comment.email}
+              content={comment.content}
+              movieName={
+                movies
+                  .filter((movie) => movie.id === comment.movieId)
+                  .map((movie) => movie.title)[0]
+              }
+              approved={comment.approved}
+            />
+          </div>
+        ))}
     </div>
   )
 }
