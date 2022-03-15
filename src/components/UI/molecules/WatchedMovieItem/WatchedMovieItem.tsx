ebea0c6614865499ddb34a68b298daffaf956bd0
Peter MX <36844547+blakbox23@users.noreply.github.com>
2022-03-15 13:40:27 +0300

    Update 'watched movies' component to get watchedMovies

--- src/components/UI/molecules/WatchedMovieItem/WatchedMovieItem.tsx
+++ src/components/UI/molecules/WatchedMovieItem/WatchedMovieItem.tsx
@@ -3,47 +3,34 @@ import Button from '../../atoms/Button/Button'
 import Text from '../../atoms/Text/Text'
 import './style.css'
 
-interface WatchedMovieItemProps {}
+interface WatchedMovieItemProps {
+  id: string
+  genre: string
+  title: string
+  image: string
+}
 
-export const WatchedMovieItem: React.FC<WatchedMovieItemProps> = ({}) => {
+export const WatchedMovieItem: React.FC<WatchedMovieItemProps> = ({
+  id,
+  genre,
+  title,
+  image,
+}) => {
   return (
     <div>
       <div className="flex watched-movie-item">
         <div className="watchedmovie-image flex">
-          <img src="https://source.unsplash.com/NX1Z62_2w04" alt="" />
+          <img src={image} alt="" />
         </div>
         <div className="watchedmovie-text flex">
           <div>
-            <Text text="Brave(2020)" type="watched-movies" font="" />
+            <Text text={title} type="watched-movies" font="" />
           </div>
           <div>
             <Text text=" ratings" type="watched-movies" font="" />
           </div>
           <div>
-            <Text text="genre" type="watched-movies" font="" />
-          </div>
-        </div>
-        <div className="delete-watchedmovie flex">
-          <Button
-            placement="watched-movies"
-            buttontext="Delete from list"
-            color="#F4242480"
-          />
-        </div>
-      </div>
-      <div className="flex watched-movie-item">
-        <div className="watchedmovie-image flex">
-          <img src="https://source.unsplash.com/NX1Z62_2w04" alt="" />
-        </div>
-        <div className="watchedmovie-text flex">
-          <div>
-            <Text text="Brave(2020)" type="watched-movies" font="" />
-          </div>
-          <div>
-            <Text text="ratings" type="watched-movies" font="" />
-          </div>
-          <div>
-            <Text text="genre" type="watched-movies" font="" />
+            <Text text={genre} type="watched-movies" font="" />
           </div>
         </div>
         <div className="delete-watchedmovie flex">
