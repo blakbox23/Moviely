e829d12b78d19f0946604e021576cff4c1bfc868
Peter MX <36844547+blakbox23@users.noreply.github.com>
2022-03-20 13:25:37 +0300

    hosting a json-server

--- server.ts
+++ server.ts
@@ -7,7 +7,7 @@ const middlewares = jsonServer.defaults()
 server.use(middlewares)
 server.use(router)
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 4000
 
 server.listen(port, () => {
     console.log(`JSON Server is running on port ${port}`)
