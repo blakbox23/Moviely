1856acf227b40f083123222db81e41440b015a0e
Peter MX <36844547+blakbox23@users.noreply.github.com>
2022-03-20 14:34:45 +0300

    Cleanup codebase

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('data/db.json')
+// const jsonServer = require('json-server')
+// const server = jsonServer.create()
+// const router = jsonServer.router('data/db.json')
 
-const middlewares = jsonServer.defaults()
+// const middlewares = jsonServer.defaults()
 
-server.use(middlewares)
-server.use(router)
+// server.use(middlewares)
+// server.use(router)
 
-const port = process.env.PORT || 4000
+// const port = process.env.PORT || 4000
 
-server.listen(port, () => {
-    console.log(`JSON Server is running on port ${port}`)
-})
+// server.listen(port, () => {
+//     console.log(`JSON Server is running on port ${port}`)
+// })
