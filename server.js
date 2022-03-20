aa0204074f2c7297b348afef59d07e3de40d1051
Peter MX <36844547+blakbox23@users.noreply.github.com>
2022-03-20 15:02:18 +0300

    Deploy

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('data/db.json')
+const cors = require("cors")
 
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 server.use(router)
+server.use(cors())
 
 const port = process.env.PORT || 4000
 
